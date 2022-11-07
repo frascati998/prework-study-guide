7486b61fc180ed28d5cddbcd9c3f7fcb08f1f2fa
frascati998 <gaia.pasqualini@hotmail.com>
2022-11-07 23:01:19 +0100

    modified

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
-var topic = "HTML";
-
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+    
+/*
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -11,7 +14,7 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-
+*/
 
 
 
