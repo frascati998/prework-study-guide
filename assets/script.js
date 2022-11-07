12d6e1e53a4faf97a1eb04a85a2163e82c08c5a0
frascati998 <gaia.pasqualini@hotmail.com>
2022-11-07 23:29:44 +0100

    modified

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
 function listTopics() {
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
@@ -19,8 +20,11 @@ if (randomTopic === 'HTML') {
   console.log('Please try again!');
  }
 }
-listTopics()
-selectTopic()
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
 
 
 
